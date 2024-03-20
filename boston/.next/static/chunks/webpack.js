a35eacfe7ce0fabc6a1b24986b0eab574fefb0a7
jensenmiers <jensenmiers@gmail.com>
2024-03-20 10:59:08 -0700

    briefer services description

--- boston/.next/static/chunks/webpack.js
+++ boston/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c8d059e3ded435d8"; }
+/******/ 		__webpack_require__.h = function() { return "5ee54327194db1d3"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
