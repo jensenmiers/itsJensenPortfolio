7b238208f7e85dfacee1974acc744aca281391f7
jensenmiers <jensenmiers@gmail.com>
2024-05-30 04:45:39 -0700

    thursday week 22

---
+++ boston/src/components/Blog.js
@@ -0,0 +1,11 @@
+// This is my Daily Blog
+// This is my journal both public and private when needed
+// I want to trigger a tweet when this blog goes active
+// I want to email a weekly recap of my blogs, codes, workouts
+
+// Today im struggling with overwhelm. I need to make more money.
+// I need to stop smoking weed. This are routinely my problems but
+// I tend to let them fester time after time and cant squash them.
+// Daily is the battle though. Just fester up courage daily.
+// You can fight this urge. This is a form of release from my mind
+// with activity on the keyboard and an outlet for my thoughts
