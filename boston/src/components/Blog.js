8128c960a2625372cf35415956629fa33556fa62
jensenmiers <jensenmiers@gmail.com>
2024-05-31 23:06:07 -0700

    fri may 31

--- boston/src/components/Blog.js
+++ boston/src/components/Blog.js
@@ -3,9 +3,17 @@
 // I want to trigger a tweet when this blog goes active
 // I want to email a weekly recap of my blogs, codes, workouts
 
+// Thu May 30
 // Today im struggling with overwhelm. I need to make more money.
 // I need to stop smoking weed. This are routinely my problems but
 // I tend to let them fester time after time and cant squash them.
 // Daily is the battle though. Just fester up courage daily.
 // You can fight this urge. This is a form of release from my mind
 // with activity on the keyboard and an outlet for my thoughts
+
+// Fri May 31
+// Another day of battle. From staying away from the weed pen, to
+// doing my best avoid binging, the daily struggle is real and unavoible. Its
+// just a strength you gott learn with time and find the ease and grace to sit through it.
+// Each day though is about striving for a tad bit more of tangible work and uncomfortable situations.
+// Tomorrow ill be sure to remind myself that the interday battle is always there.
