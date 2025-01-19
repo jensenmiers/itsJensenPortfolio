b90ac5008792ec8d88aae9f8a93b3d996eeea2fb
jensenmiers <jensenmiers@gmail.com>
2025-01-18 16:47:38 -0800

    initial commit with cleaned project

---
+++ pages/index.js
@@ -0,0 +1,59 @@
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
+import Home from "@/components/Home";
+import Portfolio from "@/components/Portfolio";
+import Services from "@/components/Services";
+import Skill from "@/components/Skill";
+// import Testimonial from "@/src/components/Testimonial";
+import ImageView from "@/components/popup/ImageView";
+import { Analytics } from "@vercel/analytics/react";
+import { boston } from "@/src/utils";
+import { Fragment, useEffect } from "react";
+const Index = () => {
+  useEffect(() => {
+    boston.scrollToActiveNav();
+    boston.imgToSvg();
+  }, []);
+
+  return (
+    <Fragment>
+      <Analytics />
+      <ImageView />
+      {/* End */}
+      {/* Header */}
+      <Header />
+      {/* End Header */}
+      {/* Main */}
+      <main className="wrapper">
+        {/* Home Section */}
+        <Home />
+        {/* End Home Section */}
+        {/* Services Section */}
+        <Services />
+        {/* End Services Section */}
+        {/* Skill Section */}
+        <Skill />
+        {/* End Skill Section */}
+        {/* Work Section */}
+        <Portfolio />
+        {/* End Work Section */}
+        {/* testimonial Section */}
+        {/* <Testimonial /> */}
+        {/* End testimonial Section */}
+        {/* Contact Section */}
+        <Contact />
+        {/* End Contact Section */}
+        {/* Effect */}
+        <div className="right-effects" />
+        <div className="left-effects" />
+        {/* End Effect */}
+      </main>
+      {/* Main */}
+      {/* Footer */}
+      <Footer />
+      {/* End Footer */}
+    </Fragment>
+  );
+};
+export default Index;
